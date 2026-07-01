824668030d61225e207681c76ba146954791c42c
Hamasullah <hamasullah.butt@nxb.com.pk>
2026-07-01 15:21:10 +0500

    signin fixed

--- app/schemas.py
+++ app/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 class UserRegister(BaseModel):
     email: str = Field(min_length=5, max_length=255)
-    password: str = Field(min_length=8, max_length=64)
+    password: str 
     language: str = "en"
     
 
